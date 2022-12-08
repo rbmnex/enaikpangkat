fa1a2d9a5704e68e5d76adff94c5460aeaab7097
haryana <haryana.dani@gmail.com>
2022-12-08 16:15:34 +0800

    commit lg

--- resources/views/mail/lampiran-mail.blade.php
+++ resources/views/mail/lampiran-mail.blade.php
@@ -5,160 +5,47 @@ body { font-family: Arial; font-size: 16.2px }
 -->
 </style>
 
-<div class="container">
-    
- <body>
-<nobr><nowrap>
-<div class="pos" id="_0:0" style="top:0">
-<div class="pos" id="_100:100" style="top:100;left:100">
-     <span id="_14" style=" font-family:Times New Roman; font-size:14px; color:#000000">
-       Salam Sejahtera</span>
-</div>
-<div class="pos" id="_100:138" style="top:138;left:100">
-      <span id="_14" style=" font-family:Times New Roman; font-size:14px; color:#000000">
-            YBhg. Dato'/ Datuk/ Datin/ YBrs. Ir./ Ar./Sr/ Dr./ Tuan/ Puan,</span>
-</div>
-<br><br>
-<div class="pos" id="_100:196" style="top:196;left:100">
-       <span id="_15" style="font-weight:bold; font-family:Times New Roman; font-size:15px; color:#000000">
-           Mohon Salinan Buku Perkhidmatan, RESUME dan Lampiran E </span>
-</div>
-<div class="pos" id="_100:215" style="top:215;left:100">
-       <span id="_17.7" style="font-weight:bold; font-family:Times New Roman; font-size:17.7px; color:#000000">
-           (PERISYTIHARAN HARTA).</span>
-</div>
-<div class="pos" id="_100:253" style="top:253;left:100">
-<span id="_17.7" style=" font-family:Times New Roman; font-size:17.7px; color:#000000">
-Dengan hormatnya saya diarah merujuk kepada perkara di atas.</span>
-</div>
-<div class="pos" id="_100:272" style="top:272;left:100">
-<span id="_16.3" style=" font-family:Times New Roman; font-size:16.3px; color:#000000">
- </span>
-</div>
-<div class="pos" id="_100:290" style="top:290;left:100">
-<span id="_17.4" style=" font-family:Times New Roman; font-size:17.4px; color:#000000">
-2.         Mohon jasa baik    YBhg. Dato'/ Datuk/ Datin/ YBrs. Ir./ Ar./Sr/ Dr./ Tuan/ </span>
-</div>
-<div class="pos" id="_100:309" style="top:309;left:100">
-<span id="_17.4" style=" font-family:Times New Roman; font-size:17.4px; color:#000000">
-Puan,   untuk mengemukakan maklumat seperti berikut <span style="font-weight:bold"> selewat-lewatnya </span></span>
-</div>
-<div class="pos" id="_100:328" style="top:328;left:100">
-<span id="_17.4" style="font-weight:bold; font-family:Times New Roman; font-size:17.4px; color:#000000">
-<span style="font-weight:normal"> untuk tindakan kami selanjutnya.</span></span>
-</div>
-<div class="pos" id="_116:367" style="top:367;left:116">
-<span id="_16.3" style=" font-family:Arial; font-size:16.3px; color:#212121">
-i.</span>
-</div>
-<div class="pos" id="_150:367" style="top:367;left:150">
-<span id="_18.5" style="font-weight:bold; font-family:Times New Roman; font-size:18.5px; color:#000000">
-Salinan Buku Perkhidmatan  (Ke alamat seperti berikut)  :</span>
-</div>
-<div class="pos" id="_150:405" style="top:405;left:150">
-<span id="_14" style=" font-family:Times New Roman; font-size:14px; color:#000000">
-Bahagian Pengurusan Sumber Manusia</span>
-</div>
-<div class="pos" id="_150:424" style="top:424;left:150">
-<span id="_14" style=" font-family:Times New Roman; font-size:14px; color:#000000">
-Cawangan Dasar dan Pengurusan Korporat</span>
-</div>
-<div class="pos" id="_150:443" style="top:443;left:150">
-<span id="_14" style=" font-family:Times New Roman; font-size:14px; color:#000000">
-Ibu Pejabat JKR Malaysia, Tingkat 29, Blok G</span>
-</div>
-<div class="pos" id="_150:462" style="top:462;left:150">
-<span id="_14" style=" font-family:Times New Roman; font-size:14px; color:#000000">
-Jalan Sultan Salahuddin 50480 Kuala Lumpur</span>
-</div>
-<div class="pos" id="_150:480" style="top:480;left:150">
-<span id="_14" style="font-weight:bold; font-family:Times New Roman; font-size:14px; color:#000000">
-(u.p. : Ir. RAHAYU BINTI MD. DIAH)  </span>
-</div>
-<div class="pos" id="_112:539" style="top:539;left:112">
-<span id="_16.3" style=" font-family:Arial; font-size:16.3px; color:#212121">
-ii.</span>
-</div>
-<div class="pos" id="_149:538" style="top:538;left:149">
-<span id="_18.6" style="font-weight:bold; font-family:Times New Roman; font-size:18.6px; color:#000000">
-Resume terkini (Mohon kemaskini data di dalam sistem My-KJ dan </span>
-</div>
-<div class="pos" id="_149:557" style="top:557;left:149">
-<span id="_18.6" style="font-weight:bold; font-family:Times New Roman; font-size:18.6px; color:#000000">
-isi lampiran 2, lampiran 3 dan lampiran 4 di dalam sistem e-naik </span>
-</div>
-<div class="pos" id="_149:576" style="top:576;left:149">
-<span id="_18.6" style="font-weight:bold; font-family:Times New Roman; font-size:18.6px; color:#000000">
-pangkat dengan menekan pautan di bawah :-</span>
-</div>
-<div class="pos" id="_149:576" style="top:576;left:149">
-<span id="_18.6" style="font-weight:bold; font-family:Times New Roman; font-size:18.6px; color:#000000">
-http://localhost:8000/user/resume/lampiran</span>
-</div>
-<div class="pos" id="_150:614" style="top:614;left:150">
-<span id="_16.3" style=" font-family:Times New Roman; font-size:16.3px; color:#000000">
- </span>
-</div>
-<div class="pos" id="_100:710" style="top:710;left:100">
-<span id="_16.3" style=" font-family:Times New Roman; font-size:16.3px; color:#000000">
---</span>
-</div>
-<div class="pos" id="_100:729" style="top:729;left:100">
-<span id="_14" style=" font-family:Times New Roman; font-size:14px; color:#000000">
-Sekian, terima kasih.</span>
-</div>
-<div class="pos" id="_100:767" style="top:767;left:100">
-<span id="_14" style="font-weight:bold; font-family:Times New Roman; font-size:14px; color:#000000">
-&#147;WAWASAN KEMAKMURAN BERSAMA 2030&#148;</span>
-</div>
-<div class="pos" id="_100:786" style="top:786;left:100">
-<span id="_16.3" style="font-weight:bold; font-family:Times New Roman; font-size:16.3px; color:#000000">
- </span>
-</div>
-<div class="pos" id="_100:805" style="top:805;left:100">
-<span id="_17.3" style="font-weight:bold; font-family:Times New Roman; font-size:17.3px; color:#000000">
-&#147;BERKHIDMAT UNTUK NEGARA&#148;</span>
-</div>
-<div class="pos" id="_100:823" style="top:823;left:100">
-<span id="_16.3" style=" font-family:Times New Roman; font-size:16.3px; color:#000000">
- </span>
-</div>
-<div class="pos" id="_100:862" style="top:862;left:100">
-<span id="_17.4" style=" font-family:Times New Roman; font-size:17.4px; color:#000000">
-Saya yang menjalankan amanah,</span>
-</div>
-<div class="pos" id="_100:919" style="top:919;left:100">
-<span id="_17.4" style="font-weight:bold; font-family:Times New Roman; font-size:17.4px; color:#000000">
-Ir. NOR EZATI BINTI BAHRIN</span>
-</div>
-<div class="pos" id="_100:957" style="top:957;left:100">
-<span id="_17.4" style=" font-family:Times New Roman; font-size:17.4px; color:#000000">
-Jurutera Awam (J44)</span>
-</div>
-<div class="pos" id="_100:976" style="top:976;left:100">
-<span id="_17.4" style="font-weight:bold; font-family:Times New Roman; font-size:17.4px; color:#000000">
-BAHAGIAN PENGURUSAN SUMBER MANUSIA (BPSM)</span>
-</div>
-<div class="pos" id="_0:0" style="top:1100">
-<img name="_1100:850" src="page_002.jpg" height="1100" width="850" border="0" usemap="#Map"></div>
-<div class="pos" id="_100:1200" style="top:1200;left:100">
-<span id="_17.3" style=" font-family:Times New Roman; font-size:17.3px; color:#000000">
-Cawangan Dasar Dan Pengurusan Korporat</span>
-</div>
-<div class="pos" id="_100:1219" style="top:1219;left:100">
-<span id="_17.3" style=" font-family:Times New Roman; font-size:17.3px; color:#000000">
-Ibu Pejabat JKR Malaysia</span>
-</div>
-<div class="pos" id="_100:1238" style="top:1238;left:100">
-<span id="_17.3" style=" font-family:Times New Roman; font-size:17.3px; color:#000000">
-Tel. No. : 03-2618 8639</span>
-</div>
-<div class="pos" id="_100:1257" style="top:1257;left:100">
-<span id="_17.3" style=" font-family:Times New Roman; font-size:17.3px; color:#000000">
-Email : <U>N</U><U>o</U><U>r</U><U>_</U><U>E</U><U>z</U><U>a</U><U>t</U><U>i</U><U>@</U><U>j</U><U>k</U><U>r</U><U>.</U><U>g</U><U>o</U><U>v</U><U>.</U><U>m</U><U>y</U></span>
-</div>
-</nowrap></nobr>
-</body>  
-                  </div>
 
-              
+                  <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header"></div>
+                                  <div class="card-body">
+                                        <p>Salam Sejahtera,</p>
+                                        <p>YBhg. Dato'/ Datuk/ Datin/ YBrs. Ir./ Ar./Sr/ Dr./ Tuan/ Puan,</p>
+                                        <p><strong>Mohon Salinan Buku Perkhidmatan, RESUME dan Lampiran E (PERISYTIHARAN HARTA).</strong></p>
+                                        <p>Dengan hormatnya saya diarah merujuk kepada perkara di atas.</p>
+                                        <p>2.		Mohon jasa baik YBhg. Dato'/ Datuk/ Datin/ YBrs. Ir./ Ar./Sr/ Dr./ Tuan/ Puan untuk mengemukakan maklumat seperti berikut selewat-lewatnya untuk tindakan kami selanjutnya.:</p>
+                                        <p>
+                                                    i. <b>Salinan Buku Perkhidmatan  (Ke alamat seperti berikut)  :</b><br/>
+                                                        Bahagian Pengurusan Sumber Manusia<br/>
+                                                        Cawangan Dasar dan Pengurusan Korporat<br/>
+                                                        Ibu Pejabat JKR Malaysia, Tingkat 29, Blok G<br/>
+                                                        Jalan Sultan Salahuddin 50480 Kuala Lumpur<br/>
+                                                        <strong>(u.p. : Ir. RAHAYU BINTI MD. DIAH)</strong>
+                                        </p>
+                                        <p>
+                                                    ii. Resume terkini (Mohon kemaskini data di dalam sistem My-KJ dan isi lampiran 2, lampiran 3 dan lampiran 4 di dalam sistem e-naik pangkat dengan menekan pautan di bawah : <br/>
+                                                    {{ $link }}
+                                        </p>
+                                        <p>Sekian, terima kasih.</p>
+                                        <p><strong>"WAWASAN KEMAKMURAN BERSAMA 2030"</strong></p>
+                                        <p><strong>"BERKHIDMAT UNTUK NEGARA"</strong></p>
+                                        <p>Saya yang menjalankan amanah,</p>
+                                        <p>
+                                            <strong>Ir. NOR EZATI BINTI BAHRIN</strong><br/>
+                                            Jurutera Awam (J44)<br/>
+                                            <strong>BAHAGIAN PENGURUSAN SUMBER MANUSIA (BPSM)</strong><br/>
+                                            Cawangan Dasar Dan Pengurusan Korporat<br/>
+                                            Ibu Pejabat JKR Malaysia<br/>
+                                            Tel. No. : 03-2618 8639<br/>
+                                            Email : <u>Nor_Ezati@jkr.gov.my</u>
+                                        </p>
+                                  </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+
+
